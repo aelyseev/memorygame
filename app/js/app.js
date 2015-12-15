65d783e7763649f0d81d0445f1ee3aa004bceb56
Aleksey Elyseev <aelyseev@gmail.com>
2015-12-15 18:59:58 +0300

    Init app

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * @author aelyseev
+ * @date 15/12/15
+ */
+
+module.exports = require('angular').module('app', ['ngRoute']);
---
+++ app/main.js
@@ -0,0 +1,8 @@
+/**
+ * @author aelyseev
+ * @date 15/12/15
+ */
+
+var app = require('./js/app');
+
+console.log(app);

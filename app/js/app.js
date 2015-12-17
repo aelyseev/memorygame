c66a628aa8a8e0a80aafd3bc703e6ceae6aca4a5
Aleksey Elyseev <aelyseev@gmail.com>
2015-12-17 15:14:02 +0300

    app

--- app/js/app.js
+++ app/js/app.js
@@ -3,4 +3,6 @@
  * @date 15/12/15
  */
 
-module.exports = require('angular').module('app', ['ngRoute', 'ngStorage']);
+module.exports = require('angular').module('app', [
+	'ngRoute', 'ngStorage'
+]);

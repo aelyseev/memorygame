c008ea899b671c0451e40f9893a4bac3b2d1a0fe
Aleksey Elyseev <aelyseev@gmail.com>
2015-12-19 00:36:23 +0300

    Change image icons generator

--- app/scripts/images.js
+++ app/scripts/images.js
@@ -1,19 +1,19 @@
 /**
- * Generate image names
+ * Image icons module generator
  * @author aelyseev
  * @date 17/12/15
  */
 
 /* eslint-disable */
-
 var fs = require('fs');
 var path = require('path');
-var Handlebars = require('handlebars');
-
-var tmpl = Handlebars.compile(fs.readFileSync(__dirname + '/css.tmpl', 'utf-8'));
-
+var tmpl = require('handlebars').compile('require(\'./{{name}}\')');
 var icons = fs.readdirSync(path.join(__dirname, '../images/icons')).filter(function (filename) {
 	return /\.svg/.test(filename);
-});
+})
+	.map(function (filename) {
+		return tmpl({name: filename})
+	});
 
-fs.writeFileSync(path.join(__dirname,  '../images/icons.css'), tmpl({files: icons}));
+fs.writeFileSync(path.join(__dirname,  '../images/icons/index.js'),
+	'/* eslint-disable  */\nmodule.exports = [\n' + icons.join(', \n') + '\n];\n');

7a9747ee215085b4ca57b393c1a232b320a04f18
Aleksey Elyseev <aelyseev@gmail.com>
2015-12-21 12:27:17 +0300

    Entry point

--- app/main.js
+++ app/main.js
@@ -4,7 +4,9 @@
  */
 
 var app = require('./js/app');
-var iconStyles = require('./images/icons.css');
+
+// styles
+require('./style/index.styl');
 
 // config
 require('./js/config');
@@ -13,48 +15,6 @@ require('./js/config');
 require('./js/menu/controller');
 require('./js/game/controller');
 
-// directives
-
-require('./style/index.styl');
-require('./js/color-generator');
-
-app.constant('puzzleMetrics', {
-	size: 55,
-	space: 8
-});
-
-app.factory('settings', [function () {
-	'use strict';
-
-	return {
-		difficulty: '2s',
-		size: '6×6'
-	};
-}]);
-
-app.controller('BoardController', ['$scope', 'settings', 'puzzleMetrics', function ($scope, Settings, puzzle) {
-	'use strict';
-	var size = Number(Settings.size[0]);
-
-	$scope.puzzle = puzzle;
-	$scope.boardSize = size * puzzle.size + (2 * puzzle.space * size);
-	$scope.puzzles = Array.apply(null, new Array(size * size)).map(function (v, i) {
-		return i + 1;
-	});
-}]);
-
-app.controller('MainController', function ($scope, $route, $routeParams, $location) {
-	'use strict';
-
-	$scope.$route = $route;
-	$scope.$location = $location;
-	$scope.$routeParams = $routeParams;
-});
-
-app.run(['$location', '$rootScope', function ($location, $rootScope) {
-	'use strict';
-
-	$rootScope.$on('$routeChangeStart', function (event, next) {
-		console.log(next.game);
-	});
-}]);
+//settings
+require('./js/services/settings');
+require('./js/services/puzzle-metrics');

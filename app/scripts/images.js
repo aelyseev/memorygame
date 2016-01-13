46d48cc043d74d9c7f05e712a6a08dab6458d7f7
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-13 14:56:38 +0300

    Make image generated script as module

--- app/scripts/images.js
+++ app/scripts/images.js
@@ -8,15 +8,19 @@
 var fs = require('fs');
 var path = require('path');
 var tmpl = require('handlebars').compile(fs.readFileSync(__dirname + '/icons.styl.tmpl', 'utf8'));
-var icons = fs.readdirSync(path.join(__dirname, '../images/icons')).filter(function (filename) {
-		return /\.svg/.test(filename);
-	})
-	.map(function (filename, id) {
-		return {style: 'icon' + id, file: filename};
-	});
 
-fs.writeFileSync(path.join(__dirname,  '../images/icons.styl'), tmpl({icons: icons}));
+module.exports = function () {
+	var icons = fs.readdirSync(path.join(__dirname, '../images/icons')).filter(function (filename) {
+			return /\.svg/.test(filename);
+		})
+		.map(function (filename, id) {
+			return {style: 'icon' + id, file: filename};
+		});
 
-fs.writeFileSync(path.join(__dirname,  '../images/icons.js'),
-	'/* eslint-disable  */\nmodule.exports = [\n' +
-	icons.map(function (v) {return JSON.stringify(v.style); }).join(', \n') + '\n];\n');
+	fs.writeFileSync(path.join(__dirname,  '../images/icons.styl'), tmpl({icons: icons}));
+
+	fs.writeFileSync(path.join(__dirname,  '../images/icons.js'),
+		'/* eslint-disable  */\nmodule.exports = [\n' +
+		icons.map(function (v) {return JSON.stringify(v.style); }).join(', \n') + '\n];\n');
+};
+

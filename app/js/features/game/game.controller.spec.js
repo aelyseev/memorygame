5a68192e1d9508b2bccc6d931ed241b526315e8e
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-14 13:49:17 +0300

    Game controller test

---
+++ app/js/features/game/game.controller.spec.js
@@ -0,0 +1,27 @@
+/**
+ * @author aelyseev
+ * @date 13/01/16
+ */
+
+/*global beforeEach, before, it*/
+
+describe('Controller: Home', function () {
+	'use strict';
+
+	var $controller;
+	var ctrl;
+	var scope;
+
+	beforeEach(angular.mock.module('app'));
+
+	beforeEach(angular.mock.inject(function (_$controller_, _$rootScope_) {
+		scope = _$rootScope_.$new();
+		$controller = _$controller_;
+		ctrl = $controller('GameController', {$scope: scope});
+	}));
+
+	it('Game Controller is defined and define a board size', function () {
+		expect(ctrl).to.be.a('object');
+		expect(scope.boardSize).to.be.a('number');
+	});
+});

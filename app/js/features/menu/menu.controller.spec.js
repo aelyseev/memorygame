17c506c289c5c99267180621c1c667499429c8dc
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-14 15:07:22 +0300

    Menu controller test

---
+++ app/js/features/menu/menu.controller.spec.js
@@ -0,0 +1,27 @@
+/**
+ * @author aelyseev
+ * @date 13/01/16
+ */
+
+/*global beforeEach, before, it*/
+
+describe('Controller: Menu', function () {
+	'use strict';
+
+	var $controller;
+	var scope;
+
+	beforeEach(angular.mock.module('app'));
+
+	beforeEach(angular.mock.inject(function (_$controller_, _$rootScope_) {
+		scope = _$rootScope_.$new();
+		$controller = _$controller_;
+		$controller('MenuController', {$scope: scope});
+	}));
+
+	it('Menu Controller is defined and define a board size', function () {
+		expect(scope.sizes).to.be.a('array');
+		expect(scope.setSize).to.be.a('function');
+		expect(scope.newgame).to.be.a('function');
+	});
+});

b195437211f2cc3127289084574fc330ffdc0709
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-22 19:42:07 +0300

    Load velocity in karma

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function (config) {
 			'node_modules/angular-mocks/angular-mocks.js',
 			'node_modules/angular-route/angular-route.js',
 			'node_modules/ngstorage/ngStorage.js',
+			'node_modules/velocity-animate/velocity.js',
 			'./app/js/test.index.js'
 		],
 

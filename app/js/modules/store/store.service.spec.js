7ddb2779804b7a5cf1ec2540c9aedae5a1ec26cf
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-24 21:46:05 +0300

    Store tests

---
+++ app/js/modules/store/store.service.spec.js
@@ -0,0 +1,15 @@
+/**
+ * @author aelyseev
+ * @date 23/01/16
+ */
+
+describe('Service: store service', function () {
+	'use strict';
+
+	var store = require('./store.service');
+	var actions = require('./store.actions');
+
+	it('Test service', function () {
+		expect(1).to.equal(1);
+	});
+});

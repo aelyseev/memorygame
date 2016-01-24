1dc5ca5a44ecac7841c1929b930a2e6bc83118d6
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-24 21:48:57 +0300

    New icons generators

---
+++ app/scripts/icons.js
@@ -0,0 +1,25 @@
+/**
+ * Image icons module generator
+ * @author aelyseev
+ * @date 17/12/15
+ */
+
+/* eslint-disable */
+var fs = require('fs');
+var path = require('path');
+var tmpl = require('handlebars').compile(fs.readFileSync(__dirname + '/icons_images.styl.tmpl', 'utf8'));
+
+module.exports = function () {
+	var icons = fs.readdirSync(path.join(__dirname, '../images/icons')).filter(function (filename) {
+			return /\.svg/.test(filename);
+		})
+		.map(function (filename, id) {
+			return {style: 'icon' + id, file: filename};
+		});
+
+	fs.writeFileSync(path.join(__dirname,  '../images/icons.styl'), tmpl({icons: icons}));
+
+	fs.writeFileSync(path.join(__dirname,  '../images/icons.js'),
+		'/* eslint-disable  */\nmodule.exports = [\n' +
+		icons.map(function (v) {return JSON.stringify(v.style); }).join(', \n') + '\n];\n');
+};

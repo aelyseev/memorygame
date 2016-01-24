36b13f051f2f72a3b5d23aa086b513b7f6b0fbaf
Aleksey Elyseev <aelyseev@gmail.com>
2016-01-24 21:23:07 +0300

    Actions

---
+++ app/js/modules/store/store.actions.js
@@ -0,0 +1,18 @@
+/**
+ * @author aelyseev
+ * @date 22/01/16
+ */
+
+module.exports = {
+	// route actions
+	GO_TO_MENU: 'GO_TO_MENU',
+	GO_TO_GAME: 'GO_TO_GAME',
+
+	// main menu
+	SET_BOARD: 'SET_BOARD',
+
+	// game actions
+	NEW_GAME: 'NEW_GAME',
+	CLICK_PUZZLE: 'CLICK_PUZZLE',
+	ROTATE_PUZZLE: 'ROTATE_PUZZLE'
+};
